926f5f33e3fa07a2333df037c56031c81b7970eb
Cassio Jhones <cassio.bjhones@gmail.com>
2023-01-19 23:29:08 -0300

    mudança iniaal

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 // buscar o id robotron e esperar o click e entao chamar a funcao dizOi 
 const robotron =  document.querySelector("#robotron");
 
-robotron.addEventListener("click", dizOi);//oque esperar/oque fazer
+robotron.addEventListener("click", function () {
+    console.log('-- ATIVAÇÃO CONCLUIDA --')
+    
+});//oque esperar/oque fazer
 
 function dizOi () {//funcao que mostra a mensagem no console
-    console.log('-- ROBO ATIVADO --')
+    console.log('-- ROBO DESATIVADO --')
+    console.log('-- Faça a ativação --')
 }
+
+dizOi();
+
+
+
+// function (){} anonima pode ser escrita  como () =>{ }
